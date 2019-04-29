ff0e51a25518f1b25b6d33df4a33e0cb62659fd5
Majbritt Westring Sørensen <B031422@PROD.SITAD.DK>
2019-04-29 14:44:27 +0200

    Forkert bemærkning. Filer slettet og genindlæst.

--- styrkefunktion.m
+++
@@ -1,9 +0,0 @@
-function beta = styrkefunktion(delta,z,sigma)
-  % STYRKEFUNKTION.m 
-  % 
-  %
-%1 - Phi( (z_crit*sigma - delta)/sigma) + Phi( (-z_crit*sigma - delta)/sigma
-    
-beta = 1 - normcdf((z.*sigma - delta)./sigma) + normcdf( (-z.*sigma - delta)./sigma);
-
-end
