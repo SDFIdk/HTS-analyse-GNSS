074d6f26f13772bc1614be0378caeb54a7f53a5b
Jonathan Emil Gundorph Jansen <jonathangundorph@gmail.com>
2020-02-24 11:48:11 +0100

    Jonathans ændringer over de sidste mange måneder.

--- TimeSeriesAnalysis.m
+++ TimeSeriesAnalysis.m
@@ -1,15 +1,16 @@
 % TIMESERIESANALYSIS
-%
+clear all; close all; clc;
+%pkg install -forge statistics This needs to be installed for some parts to work.
 % Inputs:
-% filename_input: csv file with gpsnr, refnr, X,Y,Z coordinates and
-%                 YYYYmmdd epoch (see the format string 'T' below)
-%						Note: this must be a comma seperated file with the following columns
-% 						    REFNR,GPSNR,XKOOR,YKOOR,ZKOOR,XRMS,YRMS,ZRMS,JNR_BSIDE,SYS,EPOCH,IN_DATE
-%  						    File must be sorted by REFNR in ascending order.
+%   filename_input:      csv file with gpsnr, refnr, X,Y,X coordinates and
+%                        YYYYmmdd epoch (see the formatstring 'T' below)
+%						 Note: this must be a comma seperated file with the following columns
+% 						 REFNR,GPSNR,XKOOR,YKOOR,ZKOOR,XRMS,YRMS,ZRMS,JNR_BSIDE,SYS,EPOCH,IN_DATE
+%  						 File must be sorted by REFNR in ascending order.
 %   
-%	filename_5d_points: column of four letter gnss stations that are 5d points
+%	filename_5d_points:  column of four letter gnss stations that are 5d points
 %   
-%	filename_uplifts: see readUplifts.m for file specification
+%	filename_uplifts:    see readUplifts.m for file specification
 %
 % Most settings are on/off by using 1 or 0;
 %
@@ -19,13 +20,13 @@
 %% INPUT FILENAMES:
 
 %% Data file 
-filename_input = 'inputs\\5D_tidsserier_20190820.csv';
+filename_input = 'inputs\\5D_TS_up_um.csv';
 
 %% File identifying the 5D points:
 filename_5d_points = 'inputs\\5d_points_sorted.csv'; 
 
 %% File with uplift values from DTU in mm/year
-filename_uplifts = 'inputs\\altgps_new_uplift';
+filename_uplifts = 'inputs\\altgps_uplift';
 
 %%############################################################################ 
 %% SIGNIFICANCE LEVEL:
@@ -43,7 +44,7 @@ outputENH = 0;
 %% EXCLUSION OPTION:
 %% Select which data to use
 %%
-%% 1: Use all points with 2 or more measurements (or as defined in min_points) 
+%% 1: Use all points with 3 or more measurements
 %%
 %% 2: Only use data from points designated in 5d_points file
 %% if (is5Dpoint) && (num_measurements_binned > min_points;))
@@ -53,16 +54,26 @@ outputENH = 0;
 %%
 %% 4: Only calculate for given GNSS station
 %%
-exclusionOption = 1;
+exclusionOption = 3;
 
-%% (When exclusionOption=4, only calculate for this GNSS/5D station)
-exclusiveGPSNR = 'KOKI';
+%% (When exclusionOption=4, only calculate for this GNSS station)
+exclusiveGPSNR = 'FALT';
 
 %%############################################################################
 %% MINIMUM NUMBER OF OBSERVATIONS:
 %% Set the minimum number of observation for station to get included in analysis.
-%% min_points=2 requires three observations per station.
-min_points = 1; 
+%% min_points=2 requires three observations per station. 
+min_points = 2; 
+##prompt = 'Enter minimum amount of points. Type 2 to require min. three obs. per station (default)';
+##dlgtitle = 'Input';
+%USER PROMPT MIN POINTS:
+%min_points = str2double(inputdlg(prompt,dlgtitle)); %str2double converts from cell to double.
+
+
+%NOTE! If min_points = 1, you will get alot of division with zeros in ligreg, lingregplot and TimeSeriesAnalysis (around line 414), 
+%as often the degrees of freedom will be equal to 0. (df = N-2), so if N=2, df = 0.
+
+
 
 %%############################################################################
 %% BINNING:
@@ -74,18 +85,18 @@ binsize = 14;
 %%############################################################################ 
 %% FIGURES: 
 %% Choose either figures or strength_figures (statistical strength), can't do both (bug due to octave update).
-%% 0 = no figures, 1 = show, 2 = save, 3 = show and save
-figures = 2; 
-strength_figures = 0;
+%% 0 = no figures, 1 = show, 2 = save, 3 = show and save (for both?)
+figures = 0; 
+strength_figures = 3; %cast again on lin. 456
 
 %%############################################################################
 %% MISCELLANEOUS SETTINGS: 
 %%
 %% Combining output plots:
 %% IMPORTANT: The two "combine.bat" files in folders "/figures/" and subfolder
-%%            "/figures/strength/" use GhostView
+%%            "/figures/strengh/" use GhostView
 %%            Please edit the path in the .bat files or set below to 0
-combine_plots = 0;
+combine_plots = 0; %0 by default
 
 %% Mark the middle epoch value on plots?
 plot_middle_epoch = 1;
@@ -108,7 +119,7 @@ filename_output_csv = 'outputs\\out.csv';
 filename_middle_epoch = 'outputs\\middle_epoch.csv';
 filename_output_ENH = 'outputs\\out_ENH.csv';
 filename_output_H = 'outputs\\out_H.csv';
-filename_output_geoidplot = 'outputs\\out_geoidplot.csv';
+filename_output_geoidplot = 'outputs\\out_geoidplot.csv'; %geoid output fil
 
 %%##############################################################################
 %%##############################################################################
@@ -120,7 +131,7 @@ filename_output_geoidplot = 'outputs\\out_geoidplot.csv';
 
 pkg load statistics
 
-% Formatstring (change this if the input files get different fields. %* means that the field is ignored in textread.
+% Formatstring (change this if the input files get different fields.
 T = "%d %s %f %f %f %*f %*f %*f %*d %s %s %*s";
 [REFNR, GPSNR, XKOOR, YKOOR, ZKOOR, SYS, EPOCH] =...
 textread(filename_input, T, "delimiter", ",", "headerlines", 1);
@@ -164,8 +175,9 @@ output_string_geoidplot_csv = ['GPSNR, REFNR, N, N_binned,'...  %string, int, in
                          'StdPooled observation (h),'...
                          'StdEst (dh/dt),'...
                          'StdPooled (dh/dt),'...
-                         't-test (h), z-test (h)\n'];   %int ,int          
+                         't-test (h), z-test (h)\n'];   %int ,int       
 
+%Initialize strings etc.
 output_string_H_csv = ['GPSNR, REFNR, N, N_binned,'...  %string, int, int, int  
                          'MiddleEpochDate,'...          %string
                          'MiddleEpochGPSweek,'...       %int
@@ -234,10 +246,11 @@ output_string_ENH_csv = ['GPSNR, REFNR, N, N_binned,'...
                          'ConfIntervalHigh (N),'...
                          't-test (N), z-test (N)\n'];
 
+                
 formatspec_geoidplot = ['%s, %i, %i, %i, %s, %.5f, %.5f,'...
                   '%.5f, %.5f, %.5f, %.5f, %.5f, %.5f,'...
-                  '%.5f, %.5f, %.5f, %i, %i\n'];                  
-                             
+                  '%.5f, %.5f, %.5f, %i, %i\n'];   
+              
 formatspec_H = ['%s, %i, %i, %i, %s, %i, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f,'...
                   '%.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f,'...
                   '%.5f, %.5f, %.5f, %i, %i\n'];                  
@@ -248,14 +261,27 @@ formatspec_ENH = ['%s, %i, %i, %i, %s, %i, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f,'.
                   '%i, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f,'...
                   '%i, %i, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f,'...
                   '%.5f, %i, %i\n'];
-output_middle_epoch = 'GPSNR, REFNR, Middle Epoch, X, Y, Z, N, dh/dt [mm/Year]\n';
+output_middle_epoch = 'GPSNR, REFNR, Middle Epoch, X, Y, Z, N, Slope\n';
 formatspec_middle_epoch = '%s, %i, %s, %.5f, %.5f, %.5f, %i, %.2f\n';
 
 %Read uplifts from file (see readUplifts.m for input file specification)
 uplifts = readUplifts(filename_uplifts,GPSNR(I));
 
+%Initialize waitbar
+f = waitbar(0,'1','Name','Running script...',...
+    'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
+
+setappdata(f,'canceling',0);
+
+StationTimer = tic;
+
+
+%save('preloop.m')
+%load('preloop.m')
 % For each unique in GPSNR do:
 for i = 1:length(Y);
+  %i = 65; %AFEN (To find any station in GPSNR, do index = find(strcmp(GPSNR(I), 'TRAN')) for TRAN for example).
+  %i = 101; %TRAN
   refnr = Y(i);
   gpsnr = GPSNR(I(i));
     
@@ -342,7 +368,8 @@ for i = 1:length(Y);
     northings = 1000.*northings;
   
     fprintf('...');
-      
+    %save('prelinreg_TRAN.m')
+    %load('prelinreg_TRAN.m')
     %Call linreg function
     [b, stats] = linreg(epochs, heights, alpha, uplift, sigma0_all_h);
     if outputENH
@@ -408,11 +435,12 @@ for i = 1:length(Y);
 			stats.t_test, stats.z_test_known)];
     end
 
-	  %Make list to print to the output file "middle_epoch.csv"
-	  output_middle_epoch = [output_middle_epoch sprintf(formatspec_middle_epoch,...
+	%Make list to print to the output file "middle_epoch.csv"
+	output_middle_epoch = [output_middle_epoch sprintf(formatspec_middle_epoch,...
                          gpsnr{}, refnr, datestr(epochs0,'YYYYmmdd'), X0, Y0, Z0, num_measurements, b(2))];
  
-    output_string_geoidplot_csv = [output_string_geoidplot_csv sprintf(formatspec_geoidplot,...
+      
+        output_string_geoidplot_csv = [output_string_geoidplot_csv sprintf(formatspec_geoidplot,...
     gpsnr{}, refnr, num_measurements, num_measurements_binned,...
 		datestr(epochs0,'dd-mm-YYYY'),...
     eastings0,...
@@ -423,18 +451,22 @@ for i = 1:length(Y);
 		stats.std_unknown,...
     stats.std_known,...
     stats.t_test, stats.z_test_known)];
- 
-    fprintf(['\n gpsnr: ' gpsnr{} ' done! \n'])
+      
+      fprintf(['\n gpsnr: ' gpsnr{} ' done!'])
        
   end
   
-  %Only plot figures if there are 2 or more points
+  %Only plot figures if there are 3 or more points
   if ((figures > 0) && condition)
   
+  
+
     %Set title for plots if any.
     titlestring = ['gpsnr: ' gpsnr{} ', n_{measurements}: ',...
       num2str(num_measurements) ', n_{binned}: ',...
       num2str(num_measurements_binned) ];
+      %save('linregplot_vars.m'); % i = 65 = AFEN
+      %load('linregplot_vars.m')
     fig = linregplot(epochs,heights,alpha,uplift,sigma0_all_h,show_gpsweek,...
                      plot_middle_epoch,y_limit,titlestring);
     set(fig, 'Position', get(0, 'Screensize'));
@@ -464,16 +496,11 @@ for i = 1:length(Y);
     'level ' num2str(alpha) ', N = ' num2str(num_measurements) ', N_{binned} = ' ...
     num2str(num_measurements_binned)];
     
-    %fprintf(['delta is ' num2str(delta)]);
-    fprintf(['stats.z_crit is ' num2str(stats.z_crit) '\n']);
-    fprintf(['stats.std_known is ' num2str(stats.std_known) '\n']);
-    fprintf(['alpha is ' num2str(alpha) '\n']);
-    
     fig = styrkeFunktionPlot(delta,stats.z_crit,stats.std_known,alpha,...
           styrke_title_string);     
     %Show strength curve figures
     if (strength_figures == 1) || (strength_figures == 3)
-      set(fig, 'visible', 'on');
+      set(fig, 'visible', 'off');
     end
     
     %Save strength curve figures    
@@ -488,8 +515,15 @@ for i = 1:length(Y);
       close(fig);
     end
   end
-end
+  % Update waitbar and message
+    waitbar(i/length(Y),f,sprintf('Evaluating station nr %i (%s) of %i',i, GPSNR{I(i)} ,length(Y)))
 
+  end
+  
+  close(f); %Closes the waitbar
+  
+  fprintf(['\nTimeSeriesAnalysis of all stations completed in '...
+            num2str(toc(StationTimer)) ' seconds\n']);
 
 %Combine pdf files into one
 if combine_plots > 0
@@ -525,7 +559,9 @@ fprintf(fileID,output_middle_epoch);
 fclose(fileID);
 fprintf('.CSV File written.\n')
 
+%Outputs the geoid file 
 fileID = fopen(filename_output_geoidplot,'w');
 fprintf(fileID,output_string_geoidplot_csv);
 fclose(fileID);
 fprintf('.CSV File written.\n')  
+
